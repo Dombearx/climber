1229ac5d5494bb17a850baaed8c239d0ae4e5bb5
Domin <domin.dober@gmail.com>
2024-03-17 13:23:36 +0100

    Add example data

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from pathlib import Path
 from PIL.Image import Image, open
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
 from typing import List
 
 from classes import Hold, Route
 from mocks import mock_holds_detection
+from Pylette import extract_colors
 
+def draw_bboxes(image: Image, holds: List[Hold]) -> Image:
+    # Create a drawing object
+    new_image = image.copy()
+    draw = ImageDraw.Draw(new_image, 'RGBA')
+    
+    # Define fill color with transparency
+    fill_color = (255, 0, 0, 64)  # Red with specified opacity
+    
+    # Draw each bounding box
+    for hold in holds:
+        draw.rectangle([hold.bounding_box.left, hold.bounding_box.top, hold.bounding_box.right, hold.bounding_box.bottom], fill=fill_color)
+    
+    del draw  # Release drawing object
+    return new_image
 
+def get_hold_color(image, hold):
+    image_part = image.crop([hold.bounding_box.left, hold.bounding_box.top, hold.bounding_box.right, hold.bounding_box.bottom])
+    palette = extract_colors(image_part, palette_size=3, resize=True)
+    return palette[0].rgb
 
 def read_image(image_path: Path) -> Image:
     return open(image_path)
@@ -26,11 +47,22 @@ def main():
     image_path = Path("images/example_wall.jpg")
     database = Path("")
     image = read_image(image_path)
-    holds = detect_holds(image)
-    routes = detect_routes(image, holds)
-    route = select_route(routes)
 
-    similar_routes = find_similar_routes(route, database)
+    plt.imshow(image)
+    plt.axis('off')  # Turn off axis labels
+    plt.savefig("image.jpg")
+
+    holds = detect_holds(image)
+    new_image = draw_bboxes(image, holds)
+
+    new_image.save("bboxes.jpg")
+
+    colors = [get_hold_color(image, hold) for hold in holds]
+    print(colors)
+    # routes = detect_routes(image, holds)
+    # route = select_route(routes)
+
+    # similar_routes = find_similar_routes(route, database)
 
 
 
--- mocks.py
+++ mocks.py
@@ -1,18 +1,21 @@
 from typing import List
 from PIL.Image import Image
-
+import json
 from classes import BoundingBox, Hold
 
 
 def mock_holds_detection() -> List[Hold]:
+    holds_bboxes = json.load(open("data/examples_wall.json"))
     return [
         Hold(
             bounding_box=BoundingBox(
-                top=10,
-                left=10,
-                bottom=110,
-                right=110
+                top=bbox["bounding_box"]["top"],
+                left=bbox["bounding_box"]["left"],
+                bottom=bbox["bounding_box"]["bottom"],
+                right=bbox["bounding_box"]["right"]
             ),
-            dominant_color=(255, 0, 0)
-        )
+            dominant_color=None
+        ) for bbox in holds_bboxes
     ]
+
+mock_holds_detection()
